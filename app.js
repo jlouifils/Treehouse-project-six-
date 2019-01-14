eb69cb0aee536e9f998ef1378cee199cfc15db84
jlouifils <johnnylouifils1919@gmail.com>
2019-01-13 20:19:16 -0500

    make comments

--- app.js
+++ app.js
@@ -1,36 +1,37 @@
+//- use require method for express module
 const express = require('express');
-
+//-call the exppres method
 const app = express();
-
+//require body parser, body parser and cookie parser are middleware
 const bodyParser = require('body-parser');
-
+//require cookie-Parser
 const cookieParser = require('cookie-parser');
-
+// use bodyParser in app to read post
 app.use(bodyParser.urlencoded({extended: false}));
-
+// ose cookieParser in app to store data and make server to remember data
 app.use(cookieParser());
-
+// use express.static to serve static assets
 app.use('/static',express.static(public));
-
+//told express to use pug
 app.set('view engine', 'pug');
-
+// import routs from index,js
 const routes = require('./routes/index.js');
-
+//use routes variable in a middleware function to pass in the routes
 app.use(routes);
 
-
+// catch 404 and passes to error handler
 app.use((req, res, next) =>{
   const error = new Error('Not Found');
   error.status =404;
   next(error);
 });
-
+//error handler
 app.use((error, req, res, next) =>{
     res.locals.error = error;
     res.status(error.status || 500);
     res.render('error' error);
 })
-
+//-setup the development server using the listen method
 app.listen(3000, () => {
 
 });
--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,28 @@
+//-this is my route my route file
+
+//-- use require method for express module
 const express = require('express');
-
+//-require Json file data
 const data = require('../data/projectsData.json')
-
+//-Set Routes, use router constructor to create a a router instance
 const.router = express.Router();
-
+//-An "about" route (/about) to render the "About" page
 router.get('/about',(req, res) =>{
   res.render('about');
 });
-
+//-An "index" route (/) to render the "Home" page
 router.get('/index',(req, res) =>{
   res.render('index');
   res.app.locals = data.projects;
 });
-
+//-Dynamic "project" routes (/project or /projects) based on the id of the project
+ //-that render a customized version of the Pug project template to show off each project.
 router.get('/project/:id', (req, res) => {
+  //-const reference for json file data, pass in the projects property from the data.json
   const {projects} = data;
+  //this variable represents the id of the project
   const { id } = req.params;
+
   res.render('project', {projects, id})
 });
 
